5a1b784cf7d07b7d1359bff33e6f551328293861
annadame <38460567+annadame@users.noreply.github.com>
2023-05-28 13:04:44 +0200

    Added database, database connection and controller

---
+++ SocialBrothersAssessment/SocialBrothersAssessment/Controllers/AddressController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialBrothersAssessment.Models;
+
+namespace SocialBrothersAssessment.Controllers
+{
+    [ApiController]
+    [Route("addresses")]
+    public class AddressController : ControllerBase
+    {
+        private readonly AddressDbContext _context;
+
+        public AddressController(AddressDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Address>>> GetAddresses()
+        {
+            if (_context.Addresses == null)
+            {
+                return NotFound();
+            }
+            return await _context.Addresses.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Address>> GetAddress(long id)
+        {
+            var address = await _context.Addresses.FindAsync(id);
+
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return address;
+        }
+
+        // TODO: Make Id non-changable
+        [HttpPost]
+        public async Task<ActionResult<Address>> PostAddress(Address address)
+        {
+            _context.Addresses.Add(address);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAddress), new { id = address.Id }, address);
+        }
+
+        // TODO: Make Id non-changable
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAddress(long id, Address address)
+        {
+            if (id != address.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(address).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AddressExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAddress(long id)
+        {
+            var address = await _context.Addresses.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            _context.Addresses.Remove(address);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // TODO: Try to leave out function
+        private bool AddressExists(long id)
+        {
+            return (_context.Addresses?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- SocialBrothersAssessment/SocialBrothersAssessment/Program.cs
+++ SocialBrothersAssessment/SocialBrothersAssessment/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using SocialBrothersAssessment.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add connection to Sqlite database
+var DbPath = Path.Join(Environment.CurrentDirectory, "addresses.db");
+builder.Services.AddEntityFrameworkSqlite().AddDbContext<AddressDbContext>(options => { options.UseSqlite($"Data Source={DbPath}"); });
 
 var app = builder.Build();
 
